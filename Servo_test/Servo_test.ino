5194137afe37af8f232b1b1befdf105189c0ea6f
ft-NotArt <noterisarthur42@gmail.com>
2025-03-05 01:55:36 +0100

    ~ | Some more tests

--- Servo_test/Servo_test.ino
+++ Servo_test/Servo_test.ino
@@ -4,12 +4,23 @@ Servo servo1 ;
 Servo servo2 ;
 
 void setup() {
-   servo1.attach(9);  // attache le servo1 au pin 9
-   servo2.attach(10); // attache le servo2 au pin 10
+    servo1.attach(9) ;  // attache le servo1 au pin 9
+    servo2.attach(10) ; // attache le servo2 au pin 10
+
+    servo1.write(0) ;
+    servo2.write(0) ;
+    delay(1000) ;
+
 }
 
 void loop() {
-
-  servo1.write(0) ;
-  servo2.write(0) ;
+  static int a = 0 ;
+  a += 5 ;
+  servo1.write(a) ;
+  servo2.write(a) ;
+  delay(100) ;
+  if (a == 0)
+    delay(1000) ;
+  else if (a == 180)
+    a = -5 ;
 }
